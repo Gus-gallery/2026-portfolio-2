4194a9ba07625c1b0b7e43a96cf0c4dbe55349e0
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-07-17 16:51:19 +0200

    contact component progress in links

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { useState } from 'react'
 import Contact from './components/Contact'
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [greenIsOpen, setGreenIsOpen] = useState(false)
+  const [redIsOpen, setRedIsOpen] = useState(false)
+  const [blueIsOpen, setBlueIsOpen] = useState(false)
+  const [yellowIsOpen, setYellowIsOpen] = useState(false)
 
   return (
     <main>
@@ -11,23 +14,23 @@ function App() {
         <h1 className="text-xl md:text-3xl font-light">Gustav Campbell Werdelin</h1>
         <h2 className="text-xs md:text-lg font-light text-gray-400">stud.cand.it at IT University of Copenhagen</h2>
           <div className="flex flex-row items-center justify-center pt-4 gap-4">
-            <button className="bg-square-red text-md nav-button">
+            <button onClick={() => setRedIsOpen(!redIsOpen)} className="bg-square-red text-md nav-button">
               Projects
             </button>
-            <button className="bg-square-blue text-md nav-button">
+            <button onClick={() => setBlueIsOpen(!blueIsOpen)} className="bg-square-blue text-md nav-button">
               Experience
             </button>
           </div>
           <div className="flex flex-row items-center justify-center mt-4 gap-4">
-            <button className="bg-square-yellow text-md nav-button">
+            <button onClick={() => setYellowIsOpen(!yellowIsOpen)} className="bg-square-yellow text-md nav-button">
               Education
             </button>
-            <button onClick={() => setIsOpen(!isOpen)} className="bg-square-green text-md nav-button">
+            <button onClick={() => setGreenIsOpen(!greenIsOpen)} className="bg-square-green text-md nav-button">
               Contact
             </button>
           </div>
           
-        <Contact isOpen={isOpen} />
+        <Contact greenIsOpen={greenIsOpen} onClose={() => setGreenIsOpen(false)}/>
         </div>
     </main>
   )
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,44 @@
-const Contact = ({ isOpen }) => {
+import { X } from 'lucide-react'
+
+const Contact = ({ greenIsOpen, onClose }) => {
   return (
     <div
-      className={`absolute right-8 bottom-0 h-60 w-44 md:h-80 md:w-60 bg-square-green text-primary rounded-t-md shadow-lg shadow-square-green/50 transition-all hover:saturate-300 duration-400 ease-in-out ${
-        isOpen
+      className={`absolute right-8 bottom-0 h-60 w-44 md:h-80 md:w-60 saturate-300 md:saturate-100 bg-square-green text-primary rounded-t-md shadow-lg shadow-square-green/50 transition-all hover:saturate-300 duration-400 ease-in-out ${
+        greenIsOpen
           ? 'translate-y-0 pointer-events-auto'
           : 'translate-y-full pointer-events-none'
       }`}
     >
-      <div className="p-4 flex flex-col items-left justify-left h-full w-full">
-        <h1 className="text-md md:text-xl font-light">Contact</h1>
+      <div className="p-4 flex flex-row items-start justify-between w-full">
+        <h1 className="text-md md:text-xl font-bold">Contact</h1>
+        <X onClick={onClose} className="text-primary hover:cursor-pointer transition-all rounded-full hover:text-stone-800 hover:rotate-90 ease-in-out duration-200 hover:scale-110"/>
+      </div>
+
+      <div className="p-4 flex flex-row items-center justify-center gap-2 -mt-4 ">
+          <div className="h-8 w-8 md:w-12 md:h-12 justify-around invert brightness-0 items-center
+          hover:scale-110 transition-all ease-in-out duration-400 cursor-pointer">
+            <a href="https://github.com/Gus-gallery" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/git.png" alt="Github"/>
+            </a>
+          </div>
+          <div className="h-8 w-8 md:w-12 md:h-12 justify-around invert brightness-0 items-center
+          hover:scale-110 transition-all ease-in-out duration-400 cursor-pointer">
+            <a href="https://github.com/Gus-gallery" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/mail.png" alt="E-mail"/>
+            </a>
+          </div>    
+          <div className="h-8 w-8 md:w-12 md:h-12 justify-around invert brightness-0 items-center
+          hover:scale-110 transition-all ease-in-out duration-400 cursor-pointer">
+            <a href="https://www.linkedin.com/in/gustav-werdelin-677785394/" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/link.png" alt="LinkedIn"/>
+            </a>
+          </div>
+          <div className="h-8 w-8 md:w-12 md:h-12 justify-around invert brightness-0 items-center
+          hover:scale-110 transition-all ease-in-out duration-400 cursor-pointer">
+            <a href="https://www.instagram.com/gustavwerdelin/" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/insta.png" alt="Instagram"/>
+            </a>
+          </div>
       </div>
     </div>
   )
